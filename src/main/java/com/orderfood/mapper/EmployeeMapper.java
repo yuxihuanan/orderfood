7b984e475aa53741ad93bbfb5e16bb1fe7b85c45
小璐啊小璐 <919920259@qq.com>
2018-08-25 15:26:35 +0800

    hw

--- src/main/java/com/orderfood/mapper/EmployeeMapper.java
+++ src/main/java/com/orderfood/mapper/EmployeeMapper.java
@@ -4,6 +4,6 @@ import com.orderfood.pojo.OrderfoodEmployee;
 import com.orderfood.pojo.OrderfoodStaffRole;
 import org.springframework.stereotype.Repository;
 
-@Repository("loginMapper")
+@Repository("employeeMapepr")
 public interface EmployeeMapper extends CommonMapper<OrderfoodEmployee> {
 }
