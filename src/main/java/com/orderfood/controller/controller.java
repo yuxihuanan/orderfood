def03394899267cf25cb8fc3dbb5e56a3958742b
yuxihuanan <wentao_zhao920@163.com>
2018-08-25 08:24:09 +0800

    赵文涛

--- src/main/java/com/orderfood/controller/controller.java
+++ src/main/java/com/orderfood/controller/controller.java
@@ -2,12 +2,17 @@ package com.orderfood.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class controller {
 
     @RequestMapping("index")
-    public String goIndex(){
-        return "index";
+    public ModelAndView goIndex()
+    {
+        ModelAndView model=new ModelAndView("index");
+        model.addObject("user","123456");
+        return model;
     }
+
 }
