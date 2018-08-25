f03868db727fdbd1796f59d2f17a4944c89426a1
yuxihuanan <wentao_zhao920@163.com>
2018-08-25 09:35:42 +0800

    赵文涛公共mapper

--- src/main/java/com/orderfood/controller/controller.java
+++ src/main/java/com/orderfood/controller/controller.java
@@ -1,17 +1,24 @@
 package com.orderfood.controller;
 
+import com.orderfood.service.impl.LoginServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.Resource;
+
 @Controller
 public class controller {
-
+    @Resource(name="login")
+    private LoginServiceImpl login;
     @RequestMapping("index")
     public ModelAndView goIndex()
     {
         ModelAndView model=new ModelAndView("index");
-        model.addObject("user","123456");
+        model.addObject("users",login.AdminLogin(null));
+        model.addObject("use","222");
+        model.addObject("ss",true);
         return model;
     }
 
--- src/main/java/com/orderfood/service/impl/LoginServiceImpl.java
+++ src/main/java/com/orderfood/service/impl/LoginServiceImpl.java
@@ -1,13 +1,20 @@
 package com.orderfood.service.impl;
 
+import com.orderfood.dao.LoginDao;
+import com.orderfood.mapper.CommonMapper;
+import com.orderfood.mapper.LoginMapper;
 import com.orderfood.pojo.OrderfoodStaffRole;
 import com.orderfood.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service("login")
 public class LoginServiceImpl implements LoginService {
+    @Autowired
+    private LoginMapper loginMapper;
     @Override
     public List<OrderfoodStaffRole> AdminLogin(OrderfoodStaffRole staff) {
-        return null;
+        return loginMapper.selectAll();
     }
 }
