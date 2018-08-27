0dd6b9c0be3ebbd9fe2387ba814c7b6ce21569e7
your name <your email>
2018-08-27 17:54:28 +0800

    qzc

--- src/main/java/com/orderfood/controller/IndentDetailsController.java
+++ src/main/java/com/orderfood/controller/IndentDetailsController.java
@@ -1,11 +1,12 @@
 package com.orderfood.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.orderfood.pojo.OrderfoodIndentDetails;
 import com.orderfood.service.IndentDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -19,33 +20,30 @@ public class IndentDetailsController {
      * 查看订单详情
      * @return
      */
-    @RequestMapping("getDetail")
-    public ModelAndView getDetail(){
-        ModelAndView model=new ModelAndView("");
-        model.addObject("Details",indentDetailsService.getDetail("3"));
-        return model;
+    @ResponseBody
+    @RequestMapping(value = "getDetail",produces = "text/plain;charset=utf-8")
+    public String getDetail(String tableId){
+        return JSON.toJSONString(indentDetailsService.getDetail(tableId));
     }
 
     /**
      * 修改订单详情
      * @return
      */
+    @ResponseBody
     @RequestMapping("updateDetails")
-    public ModelAndView updateDetails(OrderfoodIndentDetails orderfoodIndentDetails){
-        ModelAndView model=new ModelAndView("");
-        model.addObject("status",indentDetailsService.updateDetails(orderfoodIndentDetails));
-        return model;
+    public String updateDetails(OrderfoodIndentDetails orderfoodIndentDetails){
+        return JSON.toJSONString(indentDetailsService.updateDetails(orderfoodIndentDetails));
     }
 
     /**
      * 删除订单详情
      * @return
      */
+    @ResponseBody
     @RequestMapping("deleteDetaiils")
-    public ModelAndView deleteDetaiils(int id){
-        ModelAndView model=new ModelAndView("");
-        model.addObject("status",indentDetailsService.deleteDetaiils(id));
-        return model;
+    public String deleteDetaiils(Integer id){
+        return JSON.toJSONString(indentDetailsService.deleteDetaiils(id));
     }
 
     /**
---
+++ src/main/java/com/orderfood/controller/PageController.java
@@ -0,0 +1,18 @@
+package com.orderfood.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+    @GetMapping("tablesdetails")
+    public ModelAndView goXuanzhuo(String tableId){
+        ModelAndView model=new ModelAndView("page/tablesdetails");
+        model.addObject("tableId",3);
+        return model;
+    }
+}
