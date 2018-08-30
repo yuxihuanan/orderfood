6fd6ccb771c427fd6266a29aa39feb285a603d0a
Xin <18434054790@163.com>
2018-08-30 08:27:35 +0800

    lyx2018830 8:27

--- src/main/java/com/orderfood/controller/OrderFoodTableController.java
+++ src/main/java/com/orderfood/controller/OrderFoodTableController.java
@@ -1,9 +1,11 @@
 package com.orderfood.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.orderfood.config.RedisUtil;
 import com.orderfood.pojo.OrderfoodCuisine;
 import com.orderfood.pojo.OrderfoodTable;
+import com.orderfood.pojo.myMeum;
 import com.orderfood.service.CashierService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -36,14 +37,27 @@ public class OrderFoodTableController {
     /**
      * @Author LYX
      * 跳转页面
-     * @return 所有的桌号
+     * @return 我的菜单
      */
     @RequestMapping("MymenmShow")
     public ModelAndView MymenmShow(){
         ModelAndView modelAndView=new ModelAndView("page/Mymenu");
         String info=RedisUtil.getRu().get("lyx"+Zhuohao);
         System.out.println(info);
-        modelAndView.addObject("myMeum",info);
+        List<myMeum> list=JSONObject.parseArray(info,myMeum.class);
+        List<myMeum> list1=list;
+        for (int i = 0; i < list.size()-1; i++) {
+            for (int j = i+1; j <list1.size() ;) {
+                if(list.get(i).getName().equals(list.get(j).getName())){
+                    list.get(i).setNum(list.get(i).getNum()+1);
+                    list1.remove(j);
+                    continue;
+                }
+                j++;
+            }
+            System.out.println(list.get(i).getName()+"   "+list.get(i).getPrice()+"      "+list.get(i).getNum());
+        }
+        modelAndView.addObject("myMeum",list);
         return modelAndView;
     }
     /**
@@ -97,21 +111,11 @@ public class OrderFoodTableController {
         return modelAndView;
     }
     /**
-     * 看订单
-     * OrdermealShow
-     * @return
-     */
-    @RequestMapping("TablesdetailsShow")
-    public ModelAndView TablesdetailsShow(){
-        ModelAndView modelAndView=new ModelAndView("page/tablesdetails");
-        return modelAndView;
-    }
-    @RequestMapping("OrderTableUpadte/{tableid}/{tablenumber}/{tablestatus}")
-    /**
      * @Author LYX
      * 修改桌子信息
      * @return int
      */
+    @RequestMapping("OrderTableUpadte/{tableid}/{tablenumber}/{tablestatus}")
     public String OrderTableUpadte(@PathVariable Integer tableid,@PathVariable Integer tablenumber,@PathVariable Integer tablestatus){
         OrderfoodTable orderfoodTable=new OrderfoodTable();
         orderfoodTable.setTableid(tableid);
@@ -119,5 +123,42 @@ public class OrderFoodTableController {
         orderfoodTable.setTablestatus(tablestatus);
         int res=cashierService.Choosetable(orderfoodTable);
         return "redirect:/OrdermealShow";
+    }
+    @ResponseBody
+    @RequestMapping("tableshow")
+    /**
+     * 拿到最后下单的json数据存入json
+     */
+    public String tableshow(String sss){
+        String res=RedisUtil.getRu().set("lyx"+Zhuohao,sss);
+        return JSON.toJSONString(res);
+    }
+
+    /**
+     * @Author LYX
+     * 跳转页面下单页面
+     * @return modelandview
+     */
+    @RequestMapping("TableDetailsShow")
+    public ModelAndView TableDetailsShow(){
+        ModelAndView modelAndView=new ModelAndView("page/tablesdetails");
+        String info=RedisUtil.getRu().get("lyx"+Zhuohao);
+        System.out.println(info);
+        List<myMeum> list=JSONObject.parseArray(info,myMeum.class);
+        System.out.println(JSON.toJSONString(list));
+        modelAndView.addObject("CusineCade",list);
+        modelAndView.addObject("tableId",Zhuohao);
+        modelAndView.addObject("statu",0);
+        return modelAndView;
+    }
+
+    @RequestMapping("ZaiyongZhuo")
+    /**
+     * 加入桌子有人的话就进订单页面
+     */
+    public ModelAndView ZaiyongZhuo(){
+        ModelAndView modelAndView=new ModelAndView("page/tablesdetails");
+        modelAndView.addObject("statu",1);
+        return modelAndView;
     }
 }
---
+++ src/main/java/com/orderfood/pojo/myMeum.java
@@ -0,0 +1,40 @@
+package com.orderfood.pojo;
+
+public class myMeum {
+    private Integer id;
+    private String name;
+    private String price;
+    private Integer num;
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
