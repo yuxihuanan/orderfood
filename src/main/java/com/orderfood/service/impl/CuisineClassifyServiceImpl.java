6739d86e7584ac62aaa9f0d8250eb26aff0f0821
xuzhiwei1998 <1051766595@qq.com>
2018-08-31 18:48:57 +0800

    徐志伟提交

--- src/main/java/com/orderfood/service/impl/CuisineClassifyServiceImpl.java
+++ src/main/java/com/orderfood/service/impl/CuisineClassifyServiceImpl.java
@@ -1,11 +1,20 @@
 package com.orderfood.service.impl;
 
+import com.orderfood.mapper.CuisineClassifyMapper;
+import com.orderfood.pojo.OrderfoodCuisine;
 import com.orderfood.pojo.OrderfoodCuisineClassify;
 import com.orderfood.service.CuisineClassifyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("cuisineClassifyService")
 public class CuisineClassifyServiceImpl implements CuisineClassifyService {
+
+    @Autowired
+    private CuisineClassifyMapper cuisineClassifyMapper;
+
     @Override
     public Integer InsertCuisineClassify(OrderfoodCuisineClassify classify) {
         return null;
@@ -18,11 +27,51 @@ public class CuisineClassifyServiceImpl implements CuisineClassifyService {
 
     @Override
     public Integer DeleteCuisineClassify(Integer classifyId) {
-        return null;
+        return  null;
     }
 
     @Override
     public List<OrderfoodCuisineClassify> SelectCuisineClassify(OrderfoodCuisineClassify classify) {
-        return null;
+        return cuisineClassifyMapper.selectAll();
     }
+
+    @Override
+    public List<OrderfoodCuisineClassify> SelectCuisineClassifyById(String classifyName) {
+        System.out.println(classifyName);
+        return cuisineClassifyMapper.SelectCuisineClassifyById(classifyName);
+    }
+
+    @Override
+    public Integer del(Integer classifyid) {
+        return cuisineClassifyMapper.del(classifyid);
+    }
+
+    @Override
+    public Integer add(OrderfoodCuisineClassify orderfoodCuisineClassify) {
+        return cuisineClassifyMapper.add(orderfoodCuisineClassify);
+    }
+
+    @Override
+    public List<OrderfoodCuisineClassify> getAlls() {
+        return cuisineClassifyMapper.getAlls();
+    }
+
+    @Override
+    public Integer upd(OrderfoodCuisineClassify orderfoodCuisineClassify) {
+        return cuisineClassifyMapper.upd(orderfoodCuisineClassify);
+    }
+
+    @Override
+    public List<OrderfoodCuisineClassify> getAllbyId(Integer id) {
+        List<OrderfoodCuisineClassify> list = cuisineClassifyMapper.getAllbyId(id);
+        System.out.print(list);
+        return cuisineClassifyMapper.getAllbyId(id);
+    }
+
+    @Override
+    public Integer delpiliang(Integer classifyid) {
+        return cuisineClassifyMapper.delpiliang(classifyid);
+    }
+
+
 }
