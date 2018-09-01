51c92954d4a25ba6eb6de0c93d88df5f275d3ebf
小璐啊小璐 <919920259@qq.com>
2018-09-01 09:39:30 +0800

    黄伟更新出错别找赵文涛

--- src/main/java/com/orderfood/controller/EmployeeController.java
+++ src/main/java/com/orderfood/controller/EmployeeController.java
@@ -1,29 +1,120 @@
 package com.orderfood.controller;
 
+import com.orderfood.pojo.OrderfoodEmployee;
 import com.orderfood.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import com.alibaba.fastjson.JSON;
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class EmployeeController {
-    @Resource(name="employeeService")
+    @Autowired
     private EmployeeService employeeService;
+
+    /**
+     * 查询全部员工
+     * @return
+     */
     @RequestMapping("employeeAll")
     public ModelAndView EmployeeAll(){
         ModelAndView model=new ModelAndView("rsxz/Employlee");
         model.addObject("employees",employeeService.getAllEm());
         return model;
     }
-
+    /**
+     * 添加员工信息
+     * @param orderfoodEmployee
+     * @return
+     */
     @ResponseBody
-    @RequestMapping("employeeDel/{employeeid}")
-    public String EmployDel(@PathVariable(value = "employeeid") int employeeid){
-        return JSON.toJSONString(employeeService.delEm(employeeid));
+    @RequestMapping("employeeAdd")
+    public String EmployAdd(OrderfoodEmployee orderfoodEmployee){
+        return JSON.toJSONString(employeeService.addEm(orderfoodEmployee));
+    }
+
+    /**
+     * 修改员工信息
+     * @param orderfoodEmployee
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("employeeUpd")
+    public String EmployUpd(OrderfoodEmployee orderfoodEmployee){
+        return  JSON.toJSONString(employeeService.updEm(orderfoodEmployee));
+    }
+
+    /**
+     * 根据姓名查员工信息
+     * @param employeename
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "EmploySel/{employeename}",produces = "text/html;charset=UTF-8")
+    public String EmploySel(@PathVariable(value = "employeename")String employeename){
+        System.out.println(JSON.toJSONString(employeeService.selTwo(employeename)));
+        return JSON.toJSONString(employeeService.selTwo(employeename));
+    }
+
+    /**
+     * 批量删除
+     * @param arrays
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("delesc")
+    public String delesc(@RequestParam("arrays[]")Object[] arrays){
+        List<Object> list = new ArrayList<Object>();
+        int i = 0;
+        for (i=0;i<arrays.length;i++){
+            System.out.print(arrays[i]);
+            list.add(arrays[i]);
+        }
+       int a= employeeService.delesc(list);
+        return JSON.toJSONString(a);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * 跳转添加员工
+     * @return
+     */
+    @RequestMapping("employeeAddpage")
+    public ModelAndView EmployeeAdd(){
+        ModelAndView model=new ModelAndView("rsxz/AddEmploylee");
+        return model;
+    }
+
+    /**
+     * 传修改对象
+     * @param id
+     * @return
+     */
+    @RequestMapping("employeeUpdpage/{id}")
+    public ModelAndView EmployeeUpd(@PathVariable(value = "id") Integer id){
+        ModelAndView mode2 = new ModelAndView("rsxz/UpdEmploylee");
+        OrderfoodEmployee orderfoodEmployee=employeeService.selEm(id);
+        mode2.addObject("orderfoodEmployee",orderfoodEmployee);
+        return mode2;
     }
 }
--- src/main/java/com/orderfood/controller/EmployeeRodeController.java
+++ src/main/java/com/orderfood/controller/EmployeeRodeController.java
@@ -1,5 +1,84 @@
 package com.orderfood.controller;
 
-public class EmployeeRodeController {
+import com.alibaba.fastjson.JSON;
+import com.orderfood.pojo.OrderfoodEmployeeRole;
+import com.orderfood.service.EmployeeRoleService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class EmployeeRodeController {
+    @Autowired
+    private EmployeeRoleService employeeRoleService;
+
+    /**
+     * 查询全部角色
+     * @return
+     */
+    @RequestMapping("employeeRodeAll")
+    public ModelAndView EmployeeRodeAll(){
+        ModelAndView model=new ModelAndView("rsxz/EmployeeDetail");
+        model.addObject("employeesdetail",employeeRoleService.getAllRo());
+        return model;
+    }
+
+    /**
+     * 根据id删除角色信息
+     * @param roleid
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("employeeRoDel/{roleid}")
+    public String EmployRoDel(@PathVariable(value = "roleid") int roleid){
+        return JSON.toJSONString(employeeRoleService.delRo(roleid));
+    }
+
+    /**
+     * 添加角色信息
+     * @param orderfoodEmployeeRole
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("employeeRoAdd")
+    public String EmployRoAdd(OrderfoodEmployeeRole orderfoodEmployeeRole){
+        return JSON.toJSONString(employeeRoleService.addRo(orderfoodEmployeeRole));
+    }
+
+    /**
+     * 修改角色信息
+     * @param orderfoodEmployeeRole
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("employeeRoUpd")
+    public String EmployRoUpd(OrderfoodEmployeeRole orderfoodEmployeeRole){
+        return JSON.toJSONString(employeeRoleService.updRo(orderfoodEmployeeRole));
+    }
+
+    /**
+     * 跳转添加员工
+     * @return
+     */
+    @RequestMapping("employeeRoAddpage")
+    public ModelAndView employeeRoAddpage(){
+        ModelAndView mode2=new ModelAndView("rsxz/AddEmployeeDetail");
+        return mode2;
+    }
+    /**
+     * 跳转修改页面
+     * @param id
+     * @return
+     */
+    @RequestMapping("employeeRoUpdpage/{id}")
+    public ModelAndView EmployeeRoUpd(@PathVariable(value = "id") Integer id){
+        ModelAndView mode3 = new ModelAndView("rsxz/UpdEmployeeDetail");
+        OrderfoodEmployeeRole orderfoodEmployeeRole=employeeRoleService.selRo(id);
+        mode3.addObject("orderfoodEmployeeRole",orderfoodEmployeeRole);
+        return mode3;
+    }
 }
